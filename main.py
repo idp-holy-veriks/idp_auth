760a091912e06202e20b526e90740e66c0271b6d
n0tm3 <mihai.tecliceanu@acs.upb.ro>
2025-05-18 22:26:28 +0300

    Updates

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=os.environ["ALLOWED_ORIGINS"].split(","),
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
